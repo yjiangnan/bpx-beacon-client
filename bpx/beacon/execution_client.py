86108bc49c4eb5e3691b0ed9deece49c189613bb
bpx-network <admin@bpxcoin.cc>
2023-04-13 00:35:33 +0200

    Indent

--- bpx/beacon/execution_client.py
+++ bpx/beacon/execution_client.py
@@ -26,24 +26,26 @@ class ExecutionClient:
         exe_host: str,
         exe_port: str,
     ) -> None:
-	    if self.w3 is None:
-	        self.w3 = Web3(HTTPProvider('http://' + self.exe_host + ':' + str(self.exe_port)))
+        if self.w3 is not None:
+            return None
+            
+        self.w3 = Web3(HTTPProvider('http://' + self.exe_host + ':' + str(self.exe_port)))
 
-	        w3.eth.attach_methods({
-                "exchangeTransitionConfigurationV1": Method("engine_exchangeTransitionConfigurationV1"),
-                "forkchoiceUpdatedV2": Method("engine_forkchoiceUpdatedV2"),
-                "getPayloadV2": Method("engine_getPayloadV2"),
-                "newPayloadV2": Method("engine_newPayloadV2")
-            })
+        w3.eth.attach_methods({
+            "exchangeTransitionConfigurationV1": Method("engine_exchangeTransitionConfigurationV1"),
+            "forkchoiceUpdatedV2": Method("engine_forkchoiceUpdatedV2"),
+            "getPayloadV2": Method("engine_getPayloadV2"),
+            "newPayloadV2": Method("engine_newPayloadV2")
+        })
 
-            if not self.w3.is_connected():
-                raise RuntimeError("Cannot connect to execution client")
+        if not self.w3.is_connected():
+            raise RuntimeError("Cannot connect to execution client")
 
-            log.info("Connected to execution client")
-    
+        log.info("Connected to execution client")
+
     async def exchange_transition_configuration_task(self):
         log.info("Starting exchangeTransactionConfigurationV1 loop")
-        
+
         while True:
             self.ensure_web3_init()
             self.w3.eth.exchangeTransitionConfigurationV1({
