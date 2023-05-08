5d2cd5ae3fef578e6a4e19740e48aff759201a06
bpx-network <admin@bpxcoin.cc>
2023-05-08 14:31:25 +0200

    Revert "Payload attributes timestamp must be equal to prev tx block timestamp"
    This reverts commit 8f1a105d8a3ae8cfead5440d71e43a8fc4c3d5ea.

--- bpx/beacon/execution_client.py
+++ bpx/beacon/execution_client.py
@@ -385,7 +385,7 @@ class ExecutionClient:
             })
         
         return {
-            "timestamp": Web3.to_hex(prev_tx_block.timestamp),
+            "timestamp": Web3.to_hex(int(time.time())),
             "prevRandao": "0x" + prev_tx_block.reward_chain_block.get_hash().hex(),
             "suggestedFeeRecipient": coinbase,
             "withdrawals": raw_withdrawals,
