89dabff6b6244052d32694e74ef756f81f8fb72d
bpx-network <admin@bpxchain.cc>
2023-07-27 11:19:54 +0200

    Update chiavdf to 1.0.10

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ dependencies = [
     "aiofiles==23.1.0",  # Async IO for files
     "anyio==3.6.2",
     "blspy==2.0.2", # Signature library
-    "chiavdf==1.0.8",  # timelord and vdf verification
+    "chiavdf==1.0.10",  # timelord and vdf verification
     "chiapos==2.0.0rc1",  # proof of space
     "aiohttp==3.8.4",  # HTTP server for beacon client rpc
     "aiosqlite==0.17.0",  # asyncio wrapper for sqlite, to store blocks
