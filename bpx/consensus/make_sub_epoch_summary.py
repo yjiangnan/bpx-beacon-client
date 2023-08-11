97f0d9128ade4ca1ea8fd9e41394e1b679af3ab0
bpx-network <admin@bpxchain.cc>
2023-08-11 10:12:37 +0200

    Revert "Fix get difficulty and sub slot iters in make_sub_epoch_summary"
    This reverts commit 5157160624a7a96b4799196a7c65a6a006a1b55b.

--- bpx/consensus/make_sub_epoch_summary.py
+++ bpx/consensus/make_sub_epoch_summary.py
@@ -181,7 +181,6 @@ def next_sub_epoch_summary(
             True,
             uint128(block.total_iters - ip_iters + sp_iters - (sub_slot_iters if overflow else 0)),
             True,
-            block,
         )
         next_sub_slot_iters = _get_next_sub_slot_iters(
             constants,
@@ -194,7 +193,6 @@ def next_sub_epoch_summary(
             True,
             uint128(block.total_iters - ip_iters + sp_iters - (sub_slot_iters if overflow else 0)),
             True,
-            block,
         )
 
     return make_sub_epoch_summary(
