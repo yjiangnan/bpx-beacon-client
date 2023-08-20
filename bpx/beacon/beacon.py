d91d464535ce77d8e04a164cc005e10d185b6077
bpx-network <admin@bpxchain.cc>
2023-08-20 14:04:50 +0200

    Use fork_point in light sync

--- bpx/beacon/beacon.py
+++ bpx/beacon/beacon.py
@@ -942,7 +942,7 @@ class Beacon:
                 if self.sync_mode == "full":
                     await self._long_sync_full(fork_point, target_peak.height, target_peak.header_hash, summaries)
                 else:
-                    await self._long_sync_light(target_peak.height, target_peak.header_hash, summaries)
+                    await self._long_sync_light(fork_point, target_peak.height, target_peak.header_hash, summaries)
                     
         except asyncio.CancelledError:
             self.log.warning("Syncing failed, CancelledError")
@@ -1056,16 +1056,12 @@ class Beacon:
     
     async def _long_sync_light(
         self,
+        fork_point_height: uint32,
         peak_height: uint32,
         peak_hash: bytes32,
         summaries: List[SubEpochSummary],
     ) -> None:
         self.log.info(f"Start {self.sync_mode} syncing up to {peak_height}")
-        
-        start_height: uint32 = 0
-        peak = self.blockchain.get_peak()
-        if peak is not None:
-            start_height = peak.height + 1
         
         height_in_next_epoch = (
             peak_height + 2 * self.constants.MAX_SUB_SLOT_BLOCKS + self.constants.MIN_BLOCKS_PER_CHALLENGE_BLOCK + 5
@@ -1083,7 +1079,8 @@ class Beacon:
                 tmp_start_height = peak_height - self.constants.SUB_EPOCH_BLOCKS
                 self.log.info(f"Increased blocks count by 1 sub epoch: {tmp_start_height}")
             
-            if start_height > tmp_start_height:
+            if fork_point_height > tmp_start_height:
+                start_height = fork_point_height
                 self.log.info(f"Updated sync height to current peak + 1: {tmp_start_height}")
             
             else:
@@ -1091,6 +1088,7 @@ class Beacon:
                 self.log.info(f"Second weight proof height: {wp2_height}")
                 
                 if wp2_height < self.constants.WEIGHT_PROOF_RECENT_BLOCKS:
+                    start_height = fork_point_height
                     self.log.info("Chain too short for weight proof, using fallback start height: {start_height}")
                     
                 else:
